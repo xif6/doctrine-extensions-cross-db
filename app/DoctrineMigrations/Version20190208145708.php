3b131122404442f25ab8aeabbb79151ffb5842f7
xif6 <francois.pasini@gmail.com>
2019-02-08 16:03:12 +0100

    add migration ERROR for change ID to UUID

---
+++ app/DoctrineMigrations/Version20190208145708.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20190208145708 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE game CHANGE id id CHAR(36) NOT NULL COMMENT \'(DC2Type:guid)\'');
+        $this->addSql('ALTER TABLE gamer CHANGE game_id game_id CHAR(36) DEFAULT NULL COMMENT \'(DC2Type:guid)\'');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE game CHANGE id id INT AUTO_INCREMENT NOT NULL');
+        $this->addSql('ALTER TABLE gamer CHANGE game_id game_id INT DEFAULT NULL');
+    }
+}
