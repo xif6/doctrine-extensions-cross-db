d9d0950156ff3939bf7202788cdb5c49713193ec
xif6 <francois.pasini@gmail.com>
2019-02-08 16:03:26 +0100

    add migration SUCCESS for change ID to UUID

--- app/DoctrineMigrations/Version20190208145708.php
+++ app/DoctrineMigrations/Version20190208145708.php
@@ -18,8 +18,10 @@ class Version20190208145708 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
+        $this->addSql('ALTER TABLE gamer DROP FOREIGN KEY `FK_88241BA7E48FD905`');
         $this->addSql('ALTER TABLE game CHANGE id id CHAR(36) NOT NULL COMMENT \'(DC2Type:guid)\'');
         $this->addSql('ALTER TABLE gamer CHANGE game_id game_id CHAR(36) DEFAULT NULL COMMENT \'(DC2Type:guid)\'');
+        $this->addSql('ALTER TABLE gamer ADD CONSTRAINT `FK_88241BA7E48FD905` FOREIGN KEY (`game_id`) REFERENCES `game` (`id`)');
     }
 
     /**
@@ -30,7 +32,9 @@ class Version20190208145708 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
+        $this->addSql('ALTER TABLE gamer DROP FOREIGN KEY `FK_88241BA7E48FD905`');
         $this->addSql('ALTER TABLE game CHANGE id id INT AUTO_INCREMENT NOT NULL');
         $this->addSql('ALTER TABLE gamer CHANGE game_id game_id INT DEFAULT NULL');
+        $this->addSql('ALTER TABLE gamer ADD CONSTRAINT `FK_88241BA7E48FD905` FOREIGN KEY (`game_id`) REFERENCES `game` (`id`)');
     }
 }
