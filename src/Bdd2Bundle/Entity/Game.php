a06bc26cc9deec658d3393cd5392a200abe316b2
xif6 <francois.pasini@gmail.com>
2019-02-08 16:01:46 +0100

    change ID to UUID

--- src/Bdd2Bundle/Entity/Game.php
+++ src/Bdd2Bundle/Entity/Game.php
@@ -16,9 +16,9 @@ class Game
     /**
      * @var int
      *
-     * @ORM\Column(name="id", type="integer")
+     * @ORM\Column(name="id", type="guid")
      * @ORM\Id
-     * @ORM\GeneratedValue(strategy="AUTO")
+     * @ORM\GeneratedValue(strategy="UUID")
      */
     private $id;
 
